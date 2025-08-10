07a6ed834c80b64d00ebc0833dbccd37c1df8c53
Roshelle Perera <roshelle.enperera@gmail.com>
2025-08-10 14:43:37 +0530

    Question 02

--- Que_02.py
+++ Que_02.py
@@ -6,11 +6,15 @@ original_image = cv.imread('brain_proton_density_slice.png', cv.IMREAD_GRAYSCALE
 
 # Define control points for white matter accentuation
 cp_wm = np.array([(0, 0), (180, 0), (180, 200), (255, 200)])
-
 x = np.arange(256)
 lut_wm = np.interp(x, cp_wm[:, 0], cp_wm[:, 1]).astype('uint8')
-
 transformed_wm = cv.LUT(original_image, lut_wm)
+
+# Define control points for grey matter accentuation
+cp_gm = np.array([(0, 0), (140, 0), (140, 200), (180, 200), (180, 0), (255, 0)])
+lut_gm = np.interp(x, cp_gm[:, 0], cp_gm[:, 1]).astype('uint8')
+transformed_gm = cv.LUT(original_image, lut_gm)
+
 
 plt.figure(figsize=(10, 4))
 plt.subplot(121)
@@ -29,12 +33,6 @@ plt.axis('off')
 
 plt.show()
 
-# Define control points for grey matter accentuation
-cp_gm = np.array([(0, 0), (140, 0), (140, 200), (180, 200), (180, 0), (255, 0)])
-
-lut_gm = np.interp(x, cp_gm[:, 0], cp_gm[:, 1]).astype('uint8')
-
-transformed_gm = cv.LUT(original_image, lut_gm)
 
 plt.figure(figsize=(10, 4))
 plt.subplot(121)
